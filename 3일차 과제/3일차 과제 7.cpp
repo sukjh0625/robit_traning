daa9252fb010e6c985d984fbbc192131f98798eb
sukjh0625 <sukjh0625@gmail.com>
2026-07-15 17:36:57 +0900

    Create 3일차 과제 7.cpp

---
+++ 3일차 과제/3일차 과제 7.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main() {
+    int n, r;
+
+    printf("write n: ");
+    scanf_s("%d", &n);
+
+    printf("Write r : ");
+    scanf_s("%d", &r);
+
+
+    int a = 1;
+    for (int i = 1; i <= n; i++) {
+        a = a * i;
+    }
+
+    int b = 1;
+    for (int i = 1; i <= (n - r); i++) {
+        b = b * i;
+    }
+
+    int sunyul = a / b;
+
+
+
+    int jsunyul = 1;
+    for (int i = 0; i < r; i++) {
+        jsunyul = jsunyul * n;
+    }
+
+
+
+    int c = 1;
+    for (int i = 1; i <= r; i++) {
+        c = c * i;
+    }
+
+    int johab = a / (b * c);
+
+
+
+    int d = 1;
+    for (int i = 1; i <= (n + r - 1); i++) {
+        d = d * i;
+    }
+
+    int e = 1;
+    for (int i = 1; i <= (n - 1); i++) {
+        e = e * i;
+    }
+
+    int jjohab = d / (e * c);
+
+
+
+    printf("\nresult\n");
+    printf("nP r = %d\n", sunyul);
+    printf("nPI r = %d\n", jsunyul);
+    printf("nC r = %d\n", johab);
+    printf("nH r  = % d\n", jjohab);
+
+    return 0;
+}
