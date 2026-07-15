c16d0f0be3bb4e174e6db29a65b8f35d2441dcc0
sukjh0625 <sukjh0625@gmail.com>
2026-07-15 17:35:12 +0900

    Create 3일차 과제 5.cpp

---
+++ 3일차 과제/3일차 과제 5.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("write a value : ");
+
+   
+    if (scanf_s("%d", &n) != 1 || n <= 0) {
+        printf("enter a natural number greater than or equal to 1\n");
+        return 1;
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        for (int j = 0; j < 2 * (n - i); j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int j = 0; j < 2 * n; j++) {
+        printf("*");
+    }
+    printf("\n");
+
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        for (int j = 0; j < 2 * (n - i); j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
