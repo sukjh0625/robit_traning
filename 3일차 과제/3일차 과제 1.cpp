1990391f837456d3d6a6d5e8318b314537847c80
sukjh0625 <sukjh0625@gmail.com>
2026-07-15 17:32:55 +0900

    Create 3일차 과제 1.cpp

---
+++ 3일차 과제/3일차 과제 1.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+int main()
+{
+   
+    double num;
+    double sum = 0.0;
+    double maxv, minv;
+
+    for (int i = 1; i <= 5; ++i)
+    {
+        printf("write %d th number  ", i);
+        scanf_s("%lf", &num);
+
+        if (i == 1) {
+            maxv = num;
+            minv = num;
+        }
+        else {
+            if (num > maxv)
+            {
+                maxv = num;
+            }
+            if (num < minv)
+            {
+                minv = num;
+            }
+               }
+        sum += num;
+    }
+    double ave = sum / 5;
+
+    printf("result\n");
+    printf("average is %.6f.\n", ave);
+    printf("maximum value is %.6f.\n", maxv);
+    printf("minimum value is %.6f.\n", minv);
+
+    return 0;
+}
