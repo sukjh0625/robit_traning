2fd35ec9e0c8e977e7fccf15cc5e7f549d332d06
sukjh0625 <sukjh0625@gmail.com>
2026-07-15 17:52:59 +0900

    Update 3일차 과제 5.cpp

--- 3일차 과제/3일차 과제 5.cpp
+++ 3일차 과제/3일차 과제 5.cpp
@@ -5,40 +5,31 @@ int main() {
 
     printf("write a value : ");
 
-   
-    if (scanf_s("%d", &n) != 1 || n <= 0) {
-        printf("enter a natural number greater than or equal to 1\n");
-        return 1;
-    }
+   if (scanf_s("%d", &n) != 1 || n <= 0) 
+   {printf("enter a natural number greater than or equal to 1\n");
+        return 1; }
 
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            printf("*");
-        }
-        for (int j = 0; j < 2 * (n - i); j++) {
-            printf(" ");
-        }
-        for (int j = 0; j < i; j++) {
-            printf("*");
-        }
+    for (int i = 1; i < n; i++) 
+    { for (int j = 0; j < i; j++) 
+    {printf("*"); }
+        for (int j = 0; j < 2 * (n - i); j++) 
+        {printf(" "); }
+        for (int j = 0; j < i; j++) 
+        { printf("*");}
         printf("\n");
     }
 
-    for (int j = 0; j < 2 * n; j++) {
-        printf("*");
-    }
+    for (int j = 0; j < 2 * n; j++) 
+    { printf("*"); }
     printf("\n");
 
-    for (int i = n - 1; i >= 1; i--) {
-        for (int j = 0; j < i; j++) {
-            printf("*");
-        }
-        for (int j = 0; j < 2 * (n - i); j++) {
-            printf(" ");
-        }
-        for (int j = 0; j < i; j++) {
-            printf("*");
-        }
+    for (int i = n - 1; i >= 1; i--) 
+    { for (int j = 0; j < i; j++) 
+    { printf("*"); }
+        for (int j = 0; j < 2 * (n - i); j++) 
+        { printf(" "); }
+        for (int j = 0; j < i; j++) 
+        { printf("*"); }
         printf("\n"); 
     }
 
