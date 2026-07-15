407c90cbbcf52b78d46df868061929f1fde4f24b
sukjh0625 <sukjh0625@gmail.com>
2026-07-15 17:34:09 +0900

    Create 3일차 과제 3.cpp

---
+++ 3일차 과제/3일차 과제 3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("write year : ");
+    scanf_s("%d", &year);
+
+   
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("leap year\n");
+    }
+    else {
+        printf("common year\n");
+    }
+
+    return 0;
+}
